593a79e9324ccfaa53ef9bb8f450b81d9c1f40a7
Fırat Kazak <firatkazak@gmail.com>
2024-07-16 11:13:50 +0300

    Add project files.

---
+++ MauiProgram.cs
@@ -0,0 +1,16 @@
+﻿using Syncfusion.Maui.Core.Hosting;
+
+namespace BMI;
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        MauiAppBuilder builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>().ConfigureSyncfusionCore().ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+        });
+        return builder.Build();
+    }
+}
